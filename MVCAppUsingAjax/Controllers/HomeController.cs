807cffc6ea82db059b111fcdb731ce10dbd27320
dvenkat285 <116060501+dvenkat285@users.noreply.github.com>
2024-08-20 16:31:27 +0530

    Add project files.

---
+++ MVCAppUsingAjax/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace MVCAppUsingAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string GetScore()
+        {
+            string physicalPath = Server.MapPath("~/Matches/Score.xml");
+            var doc = XElement.Load(physicalPath);
+            string score = doc.Element("Score").Value;
+            return score;
+        }
+    }
+}
---
+++ MVCAppUsingAjax/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCAppUsingAjax.Models;
+
+namespace MVCAppUsingAjax.Controllers
+{
+    public class ProductController : Controller
+    {
+        NorthwindEntities dc = new NorthwindEntities();
+        public ActionResult Index()
+        {
+           // var products= from p in dc.Products select new {P.ProductID, P.ProductName, P.CategoryID, P.UnitPrice, P.UnitsInStock };
+            //var products = dc.Products.Select(); or
+            return View(dc.Products);
+        }
+        public ViewResult SearchProduct(string SearchTerm)
+        {
+            List<Product> Products;
+            if (string.IsNullOrEmpty(SearchTerm))
+            {
+                Products = dc.Products.ToList();
+            }
+            else
+            {
+
+                //Products = (from P in dc.Products where P.ProductName.Contains(SearchTerm) select P).ToList(); or
+                Products = dc.Products.Where(P=>P.ProductName.Contains(SearchTerm)).ToList();
+            }
+            return View("Index", Products);
+        }
+    }
+}
